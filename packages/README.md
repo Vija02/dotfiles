5116ab11ce178ca855d0f840487aae42af09b890
Vija02 <salim_michaelmi@yahoo.com>
2018-06-16 01:30:48 +0100

    feat: refactor packages category and add custom installs

---
+++ packages/README.md
@@ -0,0 +1,13 @@
+# apt-packages-essentials
+apt-packages-essentials should contain packages that are common enough to be included in the package manager  
+Will be installed before trying to install other packages  
+The idea is these packages will be needed to install the other packages
+
+# apt-packages-headless
+Should contain packages that doesn't contain GUI
+
+# apt-packages
+Contains GUI packages that I use in a day to day basis
+
+# apt-packages-bloat
+Well... Gaming and what not
--- packages/install-packages.sh
+++ packages/install-packages.sh
@@ -9,11 +9,41 @@ fi
 
 DOTFILES=$HOME/dotfiles
 
-PACKAGES=$(xargs < "$DOTFILES/packages/apt-packages-headless")
+# Install essential packages
+
+PACKAGES=$(xargs < "$DOTFILES/packages/apt-packages-essentials")
+sudo apt -fuy install $PACKAGES
+
+PACKAGES="$(xargs < "$DOTFILES/packages/apt-packages-headless")"
 if [ "$IS_HEADLESS" != 1 ]; then
   # GUI-only packages
-  PACKAGES="$PACKAGES $(xargs < "$DOTFILES/packages/apt-packages-essentials")"
-  # PACKAGES="$PACKAGES $(xargs < "$DOTFILES/packages/apt-packages")"
+  PACKAGES="$PACKAGES $(xargs < "$DOTFILES/packages/apt-packages")"
 fi
 
-sudo apt -fuy install $PACKAGES
+PACKAGES_TO_INSTALL=
+for package in $PACKAGES; do
+  if [ "$package" = "docker" ]; then
+    curl -sSL https://get.docker.com/ | sh
+    pip install docker-compose
+  elif [ "$package" = "vlc" ]; then
+    add-apt-repository ppa:videolan/master-daily
+    sudo apt update
+    sudo apt install vlc qtwayland5
+  elif [ "$package" = "chrome" ]; then
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sudo apt update
+    sudo apt install -y google-chrome-stable
+  elif [ "$package" = "vscode" ]; then
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+    sudo apt update
+    sudo apt install code
+  else
+    PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL $package"
+  fi
+done
+
+sudo apt -fuy install $PACKAGES_TO_INSTALL
