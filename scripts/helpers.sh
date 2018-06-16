ec80c8fbeb135b73bf21474ee2eb053318d1473c
Vija02 <salim_michaelmi@yahoo.com>
2018-06-16 22:34:05 +0100

    feat: script to link dotfiles

---
+++ scripts/helpers.sh
@@ -0,0 +1,28 @@
+# Common Variables {{{
+OS=$(uname)
+
+export OS
+export DOTFILES=$HOME/dotfiles
+# }}}
+
+# Colorful characters {{{
+if [ "$TERM" = 'linux' ]; then
+  ARROW='>'
+  CMARK='v '
+  INFO='i '
+  XMARK='x '
+else
+  ARROW='↪'
+  CMARK='✓'
+  INFO='ℹ'
+  XMARK='✖'
+fi
+
+if [ -t 1 ]; then
+  # Use colors in terminal
+  ARROW="$(tput setaf 2)$ARROW$(tput sgr0)"
+  CMARK="$(tput setaf 2)$CMARK$(tput sgr0)"
+  INFO="$(tput setaf 3)$INFO$(tput sgr0)"
+  XMARK="$(tput setaf 1)$XMARK$(tput sgr0)"
+fi
+# }}}
---
+++ scripts/link-dotfiles.sh
@@ -0,0 +1,37 @@
+source "$HOME/dotfiles/scripts/helpers.sh"
+
+for file in $DOTFILES/symlinks/*; do
+  target=$HOME/.`basename $file`
+  if [ -e "$target" ]; then
+    if [[ "$(readlink $target)" != "$file" ]]; then
+      if [ -d "$target" ]; then
+        echo "  $ARROW moving existing files in $target/"
+        for ofile in $target/*; do
+          echo "    $ARROW moving $ofile"
+          mv "$ofile" "$file/."
+        done
+        rmdir "$target"
+        ln -s "$file" "$target"
+        echo "$CMARK $target linked"
+      else
+        old_path=$HOME/old.`basename $file`
+        >&2 echo "$XMARK $target already exists!"
+        >&2 echo "  $ARROW Moved $target to $old_path"
+        mv "$target" "$old_path" && ln -s "$file" "$target"
+        echo "$CMARK $target linked"
+      fi
+    fi
+  else
+    ln -s "$file" "$target"
+    echo "$CMARK $target linked"
+  fi
+done
+
+if [ -n "$ERRORS" ]; then
+  # Use printf instead of echo since we have a trailing newline
+  >&2 printf "%s" "$ERRORS"
+  >&2 printf "\nOverwrite old files with '%s'\n" "${0##*/} -f"
+  exit 1
+else
+  echo "$CMARK All dotfiles files linked"
+fi
