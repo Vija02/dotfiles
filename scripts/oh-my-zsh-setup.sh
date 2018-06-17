4139800c18927fd94bae97c62fc7be07827a159a
Vija02 <salim_michaelmi@yahoo.com>
2018-06-17 23:59:50 +0100

    feat: install fonts-powerline for oh-my-zsh theme

--- scripts/oh-my-zsh-setup.sh
+++ scripts/oh-my-zsh-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt install -y zsh
+sudo apt install -y zsh fonts-powerline
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
