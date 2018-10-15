4a82f7f7673f6da16fea3f72818c5be8c8f2b818
Michael Salim <salim_michaelmi@yahoo.com>
2018-10-15 17:34:05 +0100

    feat: add dps function

--- symlinks/zsh/.zsh/functions.zsh
+++ symlinks/zsh/.zsh/functions.zsh
@@ -1,3 +1,8 @@
 function killport(){
 	sudo fuser -k $1/tcp
 }
+
+
+function dps(){ 
+	docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}\t{{.Status}}" | (read -r; printf "%s\n" "$REPLY"; sort -k 2 ) 
+}
