473ccc49359fee75fdd492d5dd11280ce7b6363c
Michael Salim <salim_michaelmi@yahoo.com>
2018-10-24 21:41:16 +0100

    chore: update url for backgrounds

--- scripts/fetch-background.sh
+++ scripts/fetch-background.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e -- discrete.tar.gz
-base=https://storage.cloud.google.com/vija02-background
+base=https://storage.googleapis.com/vija02-background
 for tar; do
     curl "$base/$tar" | tar -xvzf -
 done
