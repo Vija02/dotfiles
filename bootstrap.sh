7863b8a044a17d650502689727d7dd97c23ab455
Michael Salim <salim_michaelmi@yahoo.com>
2018-12-09 01:06:53 +0000

    feat: make bootstrap work on manjaro

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,53 @@
 set -euo pipefail
 
 # This script is to get the repo and install prerequisites.
+# Taken from get.docker.com
+# Not sure whether this will work on Ubuntu/Fedora, but I don't plan to use them anymore -w-
+get_distribution() {
+	lsb_dist=""
+	if [ -r /etc/os-release ]; then
+		lsb_dist="$(. /etc/os-release && echo "$ID")"
+	fi
+	echo "$lsb_dist"
+}
+
+install_node(){
+        # NodeJS (Current stable version should be fine)
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+        nvm install --lts
+}
+
+lsb_dist=$( get_distribution )
+
+case "$lsb_dist" in
+	manjaro) 
+		echo "$(tput setaf 3)↪ Installing yay(pacman wrapper)...$(tput sgr0)"
+		# Install yay (to replace yaourt)
+		yaourt -S --noconfirm --needed yay-bin
+
+	        echo "$(tput setaf 3)↪ Making sure packages are up to date...$(tput sgr0)"
+		yay -Syu
+		
+	        echo "$(tput setaf 3)↪ Installing some essential packages...$(tput sgr0)"
+		yay -S --noconfirm --needed curl wget make git
+
+	        echo "$(tput setaf 3)↪ Installing NodeJS stuff...$(tput sgr0)"
+		# Something was wrong with the AUR package, so lets just use the official method
+		install_node
+
+		yay -Sdd --noconfirm --needed yarn
+	;;
+
+	*)
+		echo "$(tput setaf 1)✗ Bootstrap failed. Distribution currently not supported.$(tput sgr0)"
+	        exit 1
+	;;
+esac
+
+
+: <<'END'
 
 # Check distributions and their versions
 # https://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
@@ -29,13 +76,13 @@ else
 	VER=$(uname -r)
 fi
 
-install_node(){
-	# NodeJS (Current stable version should be fine)
-	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-	nvm install --lts
-}
+#install_node(){
+#	# NodeJS (Current stable version should be fine)
+#	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+#	export NVM_DIR="$HOME/.nvm"
+#	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+#	nvm install --lts
+#}
 
 # Handle Fedora & Ubuntu
 if [ $OS = 'Fedora' ]; then
@@ -71,6 +118,8 @@ else
 	echo "$(tput setaf 1)✗ Bootstrap failed. Distribution currently not supported. Only Fedora & Ubuntu(maybe) supported$(tput sgr0)"
 	exit 1
 fi
+
+END
 
 DOTFILES=$HOME/dotfiles
 
