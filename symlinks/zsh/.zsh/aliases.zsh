cee0de86a07db0bf6dc0a4a4c09e9a44ba586598
Michael Salim <salim_michaelmi@yahoo.com>
2018-12-25 12:52:40 +0000

    feat: add alias for xclip

--- symlinks/zsh/.zsh/aliases.zsh
+++ symlinks/zsh/.zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias ll='ls -lAh'
 alias pls='sudo $(fc -ln -1)'
 alias myssh='cat ~/.ssh/id_rsa.pub'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias xclip='xclip -selection clipboard'
 
 # Other programs (needs installation)
 alias git=hub
