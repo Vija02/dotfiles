1497c1a5375e6852a9dfa024a901478a6a0d8662
Michael Salim <salim_michaelmi@yahoo.com>
2019-12-26 18:31:07 +0000

    feat: add git scripts

--- symlinks/zsh/.zsh/aliases.zsh
+++ symlinks/zsh/.zsh/aliases.zsh
@@ -5,6 +5,10 @@ alias myssh='cat ~/.ssh/id_rsa.pub'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias xclip='xclip -selection clipboard'
 
+# Git
+alias gogo='git push -u origin HEAD'
+alias fp='git fetch && git pull'
+
 # Other programs (needs installation)
 alias git=hub
 alias ranger='source ranger'
