6d355a41eb1e55c48843a3ee5ec34adb17e8726d
Michael Salim <salim_michaelmi@yahoo.com>
2021-07-02 00:22:25 +0100

    Fix: ZSH doesn't run on ubuntu

--- symlinks/zsh/.zsh/arch.zsh
+++ symlinks/zsh/.zsh/arch.zsh
@@ -1,9 +1,9 @@
 # Only run this if pacman exist
-if ! command -v pacman > /dev/null; then
-  exit 0
-fi
+if command -v pacman > /dev/null; then
 
-# JI(Just install for me pls)
-function ji(){
-  yay -S --noconfirm "$1"
-}
+  # JI(Just install for me pls)
+  function ji(){
+    yay -S --noconfirm "$1"
+  }
+
+fi
